4b524089c0a72342164aa0daf790d4a6770b4944
Lynlyn200505 <ailynlucina1727@gmail.com>
2024-09-14 14:35:12 +0800

    Create Strings.js

---
+++ Strings.js
@@ -0,0 +1,28 @@
+//Create a variable named favActorFirstName and store your favorite actor's name
+let favActorFirstName = 'Alden';
+let favActorLastName = 'Richards';
+
+//Concatenate the first and last names into a variable named fullName
+let fullName = favActorFirstName + ' ' + favActorLastName;
+
+//Create a variable named uppercase and store the lowercase version of your favorite actor's name
+let uppercase = fullName.toLowerCase();
+
+//Create a variable named lowercase and store the capitalized version of your favorite pet's name
+let petName = 'coco';
+let lowercase = 'coco'.toUpperCase();
+
+//Create a variable named message and store a message like: "My favorite actor is [FULL_NAME]".The name should be in uppercase
+let message = 'My favorite actor is: ' +fullname.toUppercase();
+
+//Append the message to include "his best show is Silicon Valley"
+message += ' and his best show is Silicon Valley.';
+
+//Log all variables to the console using the code runner in VSCode
+console.log(favActorFirstName); //Alden
+console.log(favActorLastName); // Richards
+console.log(FullName); //Alden Richards
+console.log(uppercase); //alden richards
+console.log(petName); //coco
+console.log(lowecase); //COCO
+console.log(message); //My favorite actor is: ALDEN RICHARDS and his best show is Silicon Valley.
